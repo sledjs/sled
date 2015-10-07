3b410ac2bd96fd9eb03701f5d3a3949fea0f3947
Damian <carftjr@gmail.com>
2015-10-07 22:22:16 +0200

    arrow buttons

--- views/slider.js
+++ views/slider.js
@@ -55,6 +55,18 @@ class ArrowChanger{
                     $core.modules.slides.change(1);
                     break;
             }
+class ArrowButtons {
+    constructor($core) {
+        if($core.domModules.arrows) {
+            let $arrows = $core.domModules.arrows.children,
+                slides = $core.modules.slides;
+
+            [ this.$prev, this.$next ] = [$arrows[0], $arrows[1]];
+
+            this.stop = _=> [ this.$prev, this.$next ] = null;
+
+            this.$prev.onclick = _=> slides.change(-1);
+            this.$next.onclick = _=> slides.change(1);
         }
     }
 }
