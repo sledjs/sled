3ad008d6a4513fa48724c43123ba1e3129c3bd05
rascada <carftjr@gmail.com>
2016-02-05 03:06:50 +0100

    modularize core

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
 'use strict';
 
-let log = require('@sled/log');
-let slug = require('to-slug-case');
+let Core = require('@sled/core');
+let Slides = require('@sled/slides');
 
-module.exports = class Slider {
-  constructor($slider) {
-    this.$ = $slider;
-    this.domModules = {};
-    this.modules = {};
-    this.id = $slider.id || 'slider';
-
-    log({ id: this.id }, `created`);
-    this.loadDomModules(...this.$.children);
-  }
-
-  getModule(name, type) {
-    let module;
-
-    this.module(name, (er, bundle) =>
-      module = bundle[type == '$' ? type : '_']);
-
-    return module;
-  }
-
-  module(name, cb) {
-    let bundle;
-    let err;
-
-    name = slug(name);
-
-    if (this.modules[name])
-      bundle = Object.assign(this.modules[name], { $: this.domModules[name] || null });
-    else
-      err = new Error('missing module', name);
-
-    cb && cb(err, bundle);
-    return new Promise((res, rej) => bundle ? res(bundle) : rej(err));
-  }
-
-  loadModules(...modules) {
-    if (!modules.length)
-      log(`[${this.id}]`, '[modules]', '0 modules to load');
-    else modules.forEach(Module => {
-      let name = slug(Module.name);
-      log(`[${this.id}]`, '[modules]', 'loaded', name);
-      this.modules[name] = new Module(this);
-    });
-
-    return new Promise(res => res(this));
-  }
-
-  loadDomModules(...modules) {
-    modules.forEach(domModule => {
-      this.domModules[domModule.className] = domModule;
-      log(`[${this.id}]`, '[modules-dom]', `loaded ${domModule.className}`);
-    });
-  }
+module.exports = function($slider) {
+  return new Core($slider, Slides);
 };
