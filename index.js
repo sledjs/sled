c788823f98f7e896747ea83c8ed94e7aeb393344
rascada <carftjr@gmail.com>
2016-02-07 01:36:33 +0100

    remove es6 modules

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import Core from '@sled/core';
 import Slides from '@sled/slides';
 import Keys from '@sled/keys';
 
-export default function($slider) {
+module.exports = function($slider) {
   return new Core($slider, Slides, Keys);
 };
